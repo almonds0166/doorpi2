75ed1b8642f701225e4ec6d44ddd8de920cbf0f8
Kotocade <almonds@mit.edu>
2023-04-15 21:19:52 -0400

    Progress before W20 renovation
    * fancier fonts
    * fancier color
    * slightly improved code readability

---
+++ README.md
@@ -0,0 +1,11 @@
+# doorpi
+
+The service that tracks the state of the SIPB office door.
+
+`http://doorpi.mit.edu` (requires connection to MIT Secure WiFi)
+
+Plans:
+
+* Clean up app.py
+* SSL
+* Dark mode
--- web/app.py
+++ web/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-import pymysql
 from datetime import datetime, timedelta
 from math import trunc
 from time import time_ns
-from typing import List
+
+import pymysql
+
+from typing import List, Optional, Union
 
 config_file = open("/etc/doorpi").read().splitlines()
 config = {
@@ -17,7 +19,19 @@ config = {
 db = pymysql.connect(**config, use_unicode=True, autocommit=True)
 cursor = db.cursor()
 
-def prob_open(start: datetime, end: datetime, splits: int) -> List[float]:
+def prob_open(start: datetime, end: datetime, splits: int, *, mark_future_with: Union[float, str]=0) -> List[Union[float, str]]:
+    """Probabilities that the door is open across several time slots.
+
+    Args:
+        start: The earliest time of the returned time slots.
+        end: The latest time of the returned time slots.
+        splits: The number of time slots to return.
+
+    Keyword args:
+        mark_future_with: Number or string to mark the time slots that have not
+            yet occured. Default is to mark future times with 0, representing a
+            shut door.
+    """
     # Timestamps in the database are stored in ns
     start = trunc(start.timestamp()) * 10**9
     end = trunc(end.timestamp()) * 10**9
@@ -37,7 +51,8 @@ def prob_open(start: datetime, end: datetime, splits: int) -> List[float]:
 
     relevant.extend((start + split_size * i, None) for i in range(1, splits + 1))
     # mark the door as shut for any time past now
-    relevant.append((time_ns(), 1))
+    now = time_ns()
+    relevant.append((now, 1))
     relevant.sort()
 
     open_time = 0
@@ -60,7 +75,10 @@ def prob_open(start: datetime, end: datetime, splits: int) -> List[float]:
 
         if status is None:
             # Split marker
-            probs.append(open_time / (open_time + closed_time))
+            if timestamp == now: # future
+                probs.append(mark_future_with)
+            else:
+                probs.append(open_time / (open_time + closed_time))
             open_time = 0
             closed_time = 0
 
@@ -111,7 +129,16 @@ def index():
 
     fuzzball_image = f"static/eyes_{'closed' if status else 'open'}.png"
     door_status = "closed" if status else "open"
-    prob_color = [(trunc(prob * 100), 100 - trunc(prob * 50)) for prob in hourly]
+    prob_color = []
+    for prob in hourly:
+        if isinstance(prob, float):
+            percentage = trunc(prob * 100)
+            luminescence = 100 - trunc(prob * 50)
+        else:
+            percentage = prob
+            luminescence = 100
+        prob_color.append((percentage, luminescence))
+
     return render_template("index.html",
             fuzzball_image=fuzzball_image,
             door_status=door_status,
